cd9dc2045557ace18e730e33ef07610ccb9d0629
Johnny Zhang <fd.zhonglin@gmail.com>
2017-08-20 21:20:39 -0700

    refactor: Change book to stateless component

--- src/Book.js
+++ src/Book.js
@@ -3,62 +3,54 @@ import PropTypes from 'prop-types'
 
 import * as BooksAPI from './BooksAPI'
 
-class Book extends React.Component {
-  constructor(props) {
-    super(props)
-    this.props = props
-  }
+const Book = (props) => {
+  const { title, cover, updateLibrary, authors, shelf } = props
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const newValue = event.target.value
-    const { shelf, updateLibrary } = this.props
 
     if (newValue !== shelf) {
       const oldValue = shelf
 
       updateLibrary({
-        ...this.props,
+        ...props,
         shelf: newValue
       })
 
-      BooksAPI.update({ id: this.props.id }, newValue)
+      BooksAPI.update({ id: props.id }, newValue)
         .catch(() => {
           updateLibrary({
-            ...this.props,
+            ...props,
             shelf: oldValue
           })
         })
     }
   }
 
-  render() {
-    const { title, cover, authors, shelf } = this.props
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{ width: 128, height: 193, backgroundImage: `url("${cover}")` }}
+        />
 
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{ width: 128, height: 193, backgroundImage: `url("${cover}")` }}
-          />
-
-          <div className="book-shelf-changer">
-            <select value={shelf} onChange={this.handleChange}>
-              <option value="moveTo" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-        <div className="book-title">{ title }</div>
-        <div className="book-authors">
-          { authors.join(', ') }
+        <div className="book-shelf-changer">
+          <select value={shelf} onChange={handleChange}>
+            <option value="moveTo" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
       </div>
-    )
-  }
+      <div className="book-title">{ title }</div>
+      <div className="book-authors">
+        { authors.join(', ') }
+      </div>
+    </div>
+  )
 }
 
 Book.propTypes = {
